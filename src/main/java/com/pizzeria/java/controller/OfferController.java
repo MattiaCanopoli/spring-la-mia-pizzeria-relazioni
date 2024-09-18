0d9b96f05d3c5f866726bf4fa0c9e04696e5ee47
MattiaCanopoli <canopoli.mattia@gmail.com>
2024-09-19 00:10:00 +0200

    Created read/create/delete

--- src/main/java/com/pizzeria/java/controller/OfferController.java
+++ src/main/java/com/pizzeria/java/controller/OfferController.java
@@ -1,10 +1,12 @@
 package com.pizzeria.java.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -18,25 +20,36 @@ import jakarta.validation.Valid;
 @RequestMapping("/offers")
 public class OfferController {
 
+	@Autowired
 	private OfferRepository offerRepo;
-
-	@GetMapping("/create")
-	public String create(Model model) {
-		model.addAttribute("offer", new Offer());
-		return "";
-	}
 
 	@PostMapping("/create")
 	public String store(Model model, @Valid @ModelAttribute("offer") Offer offerForm, BindingResult br,
 			RedirectAttributes ra) {
 
 		if (br.hasErrors()) {
-			return "/create";
+			return "/offers/create";
 		}
 
 		offerRepo.save(offerForm);
-		return "../pizzas/index";
+		return "redirect:/pizzas/" + offerForm.getPizza().getId();
 
+	}
+
+	@GetMapping
+	public String index(Model model) {
+		model.addAttribute("offers", offerRepo.findAll());
+		return "/offers/index";
+	}
+
+	// DELETE
+
+	@PostMapping("/delete/{id}")
+	public String delete(@PathVariable("id") Integer id, RedirectAttributes deleteMessage) {
+
+		offerRepo.deleteById(id);
+
+		return ("redirect:/offers");
 	}
 
 }
