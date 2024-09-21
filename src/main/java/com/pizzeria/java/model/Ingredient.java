829d4122a40228789ec30b23ca39b0b986ce9d4c
MattiaCanopoli <canopoli.mattia@gmail.com>
2024-09-21 04:21:18 +0200

    Create Ingredient.java

---
+++ src/main/java/com/pizzeria/java/model/Ingredient.java
@@ -0,0 +1,104 @@
+package com.pizzeria.java.model;
+
+import java.time.LocalDate;
+import java.util.Set;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "ingredient")
+public class Ingredient {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@NotNull
+	@NotEmpty
+	@Size(min = 2, max = 50)
+	@Column(name = "name", unique = true)
+	private String name;
+
+	@NotNull
+	@NotEmpty
+	@Column(name = "ingredient_family")
+	private String type;
+
+	@Column(name = "created_at")
+	private LocalDate createdAt;
+
+	@Column(name = "updated_at")
+	private LocalDate updatedAt;
+
+	@Column(name = "deleted_at")
+	private LocalDate deletedAt;
+
+	@ManyToMany(mappedBy = "ingredients")
+	private Set<Pizza> pizzas;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public LocalDate getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(LocalDate createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public LocalDate getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(LocalDate updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+
+	public LocalDate getDeletedAt() {
+		return deletedAt;
+	}
+
+	public void setDeletedAt(LocalDate deletedAt) {
+		this.deletedAt = deletedAt;
+	}
+
+	public Set<Pizza> getPizzas() {
+		return pizzas;
+	}
+
+	public void setPizzas(Set<Pizza> pizzas) {
+		this.pizzas = pizzas;
+	}
+
+}
