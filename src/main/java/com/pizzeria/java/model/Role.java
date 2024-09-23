24aac82e2e4f2f11154e7b6aace40b13d9271b4c
MattiaCanopoli <canopoli.mattia@gmail.com>
2024-09-24 00:12:25 +0200

    Create Role.java

---
+++ src/main/java/com/pizzeria/java/model/Role.java
@@ -0,0 +1,19 @@
+package com.pizzeria.java.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+public class Role {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@NotBlank
+	private String name;
+
+}
