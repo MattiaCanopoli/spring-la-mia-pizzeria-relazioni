22a9852a1c5e994045006fc7df0cc635477546bc
MattiaCanopoli <canopoli.mattia@gmail.com>
2024-09-25 00:37:05 +0200

    add: implemented UserDetails methods

--- src/main/java/com/pizzeria/java/security/DatabaseUserDetails.java
+++ src/main/java/com/pizzeria/java/security/DatabaseUserDetails.java
@@ -31,20 +31,17 @@ public class DatabaseUserDetails implements UserDetails {
 
 	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.authorities;
 	}
 
 	@Override
 	public String getPassword() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.password;
 	}
 
 	@Override
 	public String getUsername() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.username;
 	}
 
 }
