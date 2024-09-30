e51f719976e9e9f4bcb9b3a86e242c0e9b8997bd
MattiaCanopoli <canopoli.mattia@gmail.com>
2024-09-30 23:25:59 +0200

    Created update() and delete()

--- src/main/java/com/pizzeria/java/api/PizzaRestController.java
+++ src/main/java/com/pizzeria/java/api/PizzaRestController.java
@@ -7,9 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -56,8 +58,34 @@ public class PizzaRestController {
 	public Pizza store(@RequestBody Pizza pizza) {
 
 		Pizza newPizza = pService.save(pizza);
-
 		return newPizza;
+
+	}
+
+	@PutMapping("/{id}")
+	public ResponseEntity<Pizza> update(@PathVariable("id") Integer id, @RequestBody Pizza pizza) {
+
+		Optional<Pizza> foundPizza = pService.findById(id);
+
+		if (foundPizza.isPresent()) {
+			pizza = pService.update(pizza, id);
+			return new ResponseEntity<>(pizza, HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> delete(@PathVariable("id") Integer id) {
+		Optional<Pizza> foundPizza = pService.findById(id);
+
+		if (foundPizza.isPresent()) {
+			pService.delete(id);
+			return new ResponseEntity<>(HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
 
 	}
 
